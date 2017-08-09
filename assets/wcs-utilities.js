bac2bc4b453745b245e19870eca52cad413bc974
andytilia <andytilia@gmail.com>
2017-08-09 11:15:02 -0400

    add testbank embed. supports #34

--- assets/wcs-utilities.js
+++ assets/wcs-utilities.js
@@ -1,3 +1,12 @@
 $.get("navbar.html", function(data){
   $("#navbar").replaceWith(data);
 });
+
+$('#slidesModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var displayUrl = "https://docs.google.com/a/wcsu.net/presentation/d/" + button.data('slides-id') + "/embed?start=false&loop=true&delayms=10000";// Extract info from data-* attributes
+  var editUrl = "location.href='https://docs.google.com/presentation/d/" + button.data('slides-id') + "/edit'";// Extract info from data-* attributes
+  var modal = $(this)
+  $('#slidesIframe').attr('src',displayUrl);
+  $('#slidesEditButton').attr('onClick',editUrl);
+})
